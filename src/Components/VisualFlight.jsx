308d51bbe2e47a964d9231a4f0019e9c2faf9784
Yehuda Briskman <yr0556772363@gmail.com>
2024-04-04 08:04:44 +0300

    update text

--- src/Components/VisualFlight.jsx
+++ src/Components/VisualFlight.jsx
@@ -28,7 +28,7 @@ const VisualFlight = () => {
     return (
         <div className='pb-5 max-w-7xl mx-auto'>
             <div className='w-full flex justify-center'>
-                <button className='m-3 mb-10 bg-gradient-to-r from-slate-400 via-gray-400 to-slate-400 shadow-lg text-black hover:scale-110 transition duration-300 ease-in-out font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline' onClick={() => setShowFlyData(true)}>TEXT</button>
+                <button className='m-3 mb-10 bg-gradient-to-r from-slate-400 via-gray-400 to-slate-400 shadow-lg text-black hover:scale-110 transition duration-300 ease-in-out font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline' onClick={() => setShowFlyData(true)}>Text</button>
                 <button className='m-3 mb-10 bg-gradient-to-r from-slate-400 via-gray-400 to-slate-400 shadow-lg text-black hover:scale-110 transition duration-300 ease-in-out font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline' onClick={() => setShowFlyData(false)}>Visual</button>
                 <button className='m-3 mb-10 bg-gradient-to-r from-slate-400 via-gray-400 to-slate-400 shadow-lg text-black hover:scale-110 transition duration-300 ease-in-out font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline' onClick={() => nav("/flightDetails")}><FaPlus /></button>
             </div>
